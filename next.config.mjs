003894023f82621f0a575e9b2a8105a0b8794628
karan-fd <sonik1486@gmail.com>
2024-04-25 19:58:26 +0545

    Project Setup boilerplate

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  images: {
+    unoptimized: true,
+    domains: ['localhost'],
+  },
+  env: {},
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 's-maxage=600, stale-while-revalidate=30',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
