16c294ec129df3db754e7cede1a41e085968781e
karan-fd <sonik1486@gmail.com>
2024-04-26 06:08:03 +0545

    resturant static filter updated

--- src/app/resturants/page.tsx
+++ src/app/resturants/page.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { CategoryList } from './partials/CategoryList';
+import { CategoryList, Range, Rating } from './partials';
 
 const Resturant: React.FC = () => {
   return (
     <section className="resturant--section">
       <div className="container">
-        <div className="grid">
-          <aside className="">
+        <div className="grid xs:grid-cols-4">
+          <aside className="grid gap-7 xs:col-span-1">
             <CategoryList />
+            <Range />
+            <Rating />
           </aside>
-          <main className=""></main>
+          <main className="xs:col-span-3"></main>
         </div>
       </div>
     </section>
--- src/app/resturants/partials/CategoryList.tsx
+++ src/app/resturants/partials/CategoryList.tsx
@@ -1,23 +1,20 @@
 'use client';
-import { ICategory } from '@models/category';
-import { CateData } from '@shared/category-data';
+import { CateData } from '@shared/resturant-data';
 import Image from 'next/image';
 import React, { useState } from 'react';
 
 export const CategoryList: React.FC = () => {
-  //   const categories = CateData;
-  const [categories, setCategories] = useState<ICategory[]>(CateData);
   const [selectedCate, setSelectedCate] = useState<string | number>(0);
   return (
     <div>
-      <h4 className="text-lg font-semibold md:text-xl">Select Food Type:</h4>
-      <ul className="mt-5 grid grid-cols-[repeat(auto-fill,_minmax(150px,1fr))] gap-3">
-        {categories &&
-          categories.map((item, idx) => {
+      <h4 className="text-lg font-semibold md:text-xl">Food Type:</h4>
+      <ul className="mt-5 grid grid-cols-[repeat(auto-fill,_minmax(125px,1fr))] gap-3">
+        {CateData &&
+          CateData.map((item, idx) => {
             return (
               <li
                 key={item.id}
-                className={`flex cursor-pointer flex-col items-center justify-center rounded-xl border border-solid border-transparent bg-gray-100 px-2 py-5 grayscale hover:grayscale-0 ${selectedCate === idx ? 'border-purple-200 bg-purple-50 grayscale-0' : null}`}
+                className={`flex cursor-pointer flex-col items-center justify-center rounded-xl border border-solid bg-gray-100 px-2 py-5 grayscale hover:grayscale-0 ${selectedCate === idx ? 'border-purple-200 bg-purple-50 grayscale-0' : 'border-transparent'}`}
                 onClick={() => setSelectedCate(idx)}
               >
                 <figure>
